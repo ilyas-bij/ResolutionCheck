cd66c55baefdc6bc902b8719b4f5777ac3a2ca17
Ilyas Bijediguen <ibijediguen@masaratapp.com>
2022-07-06 13:01:13 +0100

    new

--- pages/Home.js
+++ pages/Home.js
@@ -1,11 +1,10 @@
 import React,{useState,useEffect} from "react";
 import { StyleSheet, Text, View,Image ,Pressable} from "react-native";
+import {launchImageLibrary} from 'react-native-image-picker';
+import DefaultImage from './images.jpg';
 
-//import ImagePicker from 'react-native-image-picker';
-import { launchImageLibrary} from 'react-native-image-picker';
-//import MediaMeta from 'react-native-media-meta';
-import Exif from 'react-native-exif'
-
+import axios from "axios";
+const DEFAULT_IMAGE = Image.resolveAssetSource(DefaultImage).uri;
 
 
 
@@ -13,21 +12,28 @@ import Exif from 'react-native-exif'
 export default function Home() {
 
   const [filePath, setFilePath] = useState();
-
+  const [quality,setquality] = useState();
 
  
   const chooseFile = () => {
+
+
+ 
+
     let options = {
       title: 'Select Image',
+      selectionLimit: 1,
+      mediaType: 'photo',
+      quality: 1,
+      exif: true,
+
       storageOptions: {
         skipBackup: true,
         path: 'images',
       },
     };
     launchImageLibrary(options, (response) => {
-      console.log('response', response);
-
-     
+      //console.log('response', response);
       if (response.didCancel) {
         console.log('User cancelled image picker');
       } else if (response.error) {
@@ -36,31 +42,63 @@ export default function Home() {
         console.log('User tapped custom button: ', response.customButton);
         alert(response.customButton);
       } else {
-        const source = { uri: response.uri };
-        console.log('response', response);
+        //console.log('response', response);
         setFilePath(response.assets[0])
-       
-      }
+        setquality()
+        let img =response.assets[0]
+        const formData = new FormData();
+        formData.append('file', {
+                                  uri: img.uri, //Your Image File Path
+                                type: img.type, 
+                                name: img.fileName,
+                              });
+
+        //console.log("formData,",img);
+        // url: "http://10.0.2.2:5000/upload",
+        axios({
+          url: "http://ilyasbij.pythonanywhere.com/upload",
+          method : 'POST',
+          data   : formData,
+          headers: {
+                       Accept: 'application/json',
+                       'Content-Type': 'multipart/form-data',
+                       'Authorization':'Basic YnJva2VyOmJyb2tlcl8xMjM='
+                   }
+               })
+               .then(function (response) {
+                       console.log("response :", response.data);
+                       setquality(response.data.quality)
+              })
+              .catch(function (error) {
+                       console.log("error from image :",error);
+              })
+
+       }
      });
+
+   
      
   };
 
+  
+
+
+
   useEffect(() => {
     const removeFirst =filePath &&  filePath.uri.replace('file://', '');
-    console.log(removeFirst); // 👉️ ",one,two"
-    /* filePath && console.log('uri ', filePath.uri);
-    filePath && MediaMeta.get(removeFirst)
-        .then(metadata =>{
-          delete metadata.thumb ; 
-          console.log(metadata )
-        })
-        .catch(err => console.error(err));*/
-//-------------------------------------------
-        filePath &&  Exif.getExif(filePath.uri)
-        .then(msg => console.warn(msg))
-        .catch(msg => console.warn('ERROR: ' + msg))
+    //console.log(removeFirst); // 👉️ ",one,two"
+    
+    // var formData = new FormData();
+    // formData.append("file", filePath);
+
+   
+    //console.log(filePath);
+
       
   },[filePath]);
+
+ 
+ 
  
 
   return (
@@ -68,20 +106,21 @@ export default function Home() {
 
     <Image
       style={styles.img}
-      source={filePath ? {uri: filePath.uri} :{uri: ''}}
+      source={quality ? {uri: filePath.uri} :{uri: DEFAULT_IMAGE}  
+    }
     />
      <View>
-      <Text style={styles.title}>image info</Text>
-      <Text> pixels    :{filePath && filePath.width * filePath.height } px </Text>
-      <Text >width    : {filePath && filePath.width} </Text>
-      <Text >height   : {filePath && filePath.height} </Text>
-      <Text >size     : {filePath && Number(filePath.fileSize)}  </Text>
-      <Text >url      : {filePath && filePath.uri}  </Text>
-      
- </View>
-  
- 
- 
+        <Text style={styles.title}>image info</Text>
+        <Text>pixels   :{quality && `${filePath.width * filePath.height}px` }  </Text>
+        <Text >width    : {quality && filePath.width} </Text>
+        <Text >height   : {quality && filePath.height} </Text>
+        <Text >size     : {quality && Number(filePath.fileSize)}  </Text>
+        {quality >40 && quality <60 ? <Text style={{color:'#ffc107'}}>image quality  : {quality && quality }</Text>
+         :quality <40 ? <Text style={{color:'red'}} >image quality  : {quality && quality }</Text> 
+        : quality >60 ?<Text style={{color:'#4caf50'}}>image quality  : {quality && quality }</Text> 
+      : <Text >image quality  : {quality && quality }</Text> }
+        
+    </View>
         <Pressable style={styles.btn} onPress={chooseFile}>
                 <Text  style={styles.titlebtn}>upload</Text>
         </Pressable>
@@ -101,8 +140,9 @@ const styles = StyleSheet.create({
         backgroundColor: "#eaeaea"
       },
       img:{
-        width: 340, 
+        width: '100%', 
         height: 360,
+        marginBottom:2
       },
       btn: {
         paddingVertical: 8,
